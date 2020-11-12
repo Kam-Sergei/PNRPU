26de9bfbac2549f1b822a4628f6c8fa500b472bf
Kam-Sergei <73437904+Kam-Sergei@users.noreply.github.com>
2020-11-13 01:14:25 +0500

    Add files via upload

---
+++ 2_5/Код.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int n; //n-длина катета.
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	cout << "¬ведите длину катета" << endl;
+	cin >> n;
+	if (n <= 2)
+	{
+		cout << "ќшибка! (не удовлетвор€ет условию задачи)";
+	}
+	else
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			for (int j = i; j < n; j++)
+			{
+				cout << "  ";
+			}
+			for (int j = 1; j <= i; j++)
+			{
+				cout<<"* ";
+			}
+			cout << endl;
+		}
+	}
+	return 0;
+}
