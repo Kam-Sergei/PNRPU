91f3a18156acb1483e3230b60d59dc14aa149786
Kam-Sergei <73437904+Kam-Sergei@users.noreply.github.com>
2020-12-12 02:28:10 +0500

    Add files via upload

---
+++ 1 семестр/2_19/Код.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int n, s; //n-количество слагаемых. s-сумма.
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	cout << "Введите количество слагаемых (n) : ";
+	cin >> n;
+	s = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		if (i % 3 == 0)
+		{
+			s -= i;
+		}
+		else
+		{
+			s += i;
+		}
+	}
+	cout << "Сумма (s) = " << s;
+	return 0;
+}
