02551eb4df8dbd644e7cc0614a602fb032728269
Kam-Sergei <73437904+Kam-Sergei@users.noreply.github.com>
2020-12-15 14:04:57 +0500

    Add files via upload

---
+++ 1 семестр/Лабораторные/Лабораторная 7/Код.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+int n,a,b; //n-n.
+void f(int n)
+{
+	if (n % a != 0 or n % b != 0)
+	{
+		n++;
+		f(n);
+	}
+	else
+	{
+		cout <<"НОК ("<<a<<";"<<b<<") = "<< n;
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	cout << "Введите первое число a: a = ";
+	cin >> a;
+	cout << "Введите второе число b : b = ";
+	cin >> b;
+	n = 1;
+	f(n);
+	return 0;
+}
